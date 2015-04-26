b1bb69c04709c35be792b7f695fca5bf432059e5
Thibault Milan <hello@thibaultmilan.com>
2015-04-26 13:59:19 +0200

    change the default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require('gulp');
 var wiredep = require('wiredep');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+gulp.task('default', ['bower','sass']);
 
 gulp.task('bower', function () {
 	wiredep({ src: 'index.html' });
