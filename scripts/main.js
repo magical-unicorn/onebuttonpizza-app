5ae3ffaa679e7038014b2541d8bb4c904b4a0420
biou <biou@sous-anneau.org>
2015-04-26 14:36:14 +0200

    populate templates

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
  $(document).ready(function() {
-
+ 	//var app = {user: {firstname: "Thibault", lastname: "Milan", adress: "5, rue des Trinitaires, Metz, France", phone: "+33679889177", cb: {lastdigit: "6789", expire: "03/16"}, pizza: "Margherita"}};
+ 	//localStorage.setItem('app', JSON.stringify(app));
+ 	updateTemplates();
  });
+
+ function showMenu() {	
+  	$('.oc-container').addClass('oc-menu-open');
+ }
+
+ function updateTemplates() {
+ 	$('div.templatized').remove();
+ 	var app = JSON.parse(localStorage.getItem('app'));
+
+	var source   = $('script[type="text/x-handlebars-template"]');
+	source.each(function(i) {
+		var elem = source[i];
+		var content = '<div class="templatized">'+Handlebars.compile($(elem).html())(app)+'</div>';
+		//console.log(content);
+		$(elem).after(content);
+	});
+
+ }
